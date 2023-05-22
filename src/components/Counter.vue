52f96716f929759830125e3f7bd36a8c66c25bf4
branmeco <branmeco17@gmail.com>
2023-05-22 10:57:49 -0500

    Propiedades computadas - Computed Properties

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,11 @@
 <template>
   <h2>Counter</h2>
   <p>{{counter}} <sup>2</sup> = {{counter * counter}}</p>
+  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
+  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
+  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
+  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
+  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
 </template>
 
 <script>
@@ -9,6 +14,18 @@ export default {
     data(){
         return {
             counter: 5
+        }
+    },
+    methods: {
+        getSquareValue(){
+            console.log('getSquareValue')
+            return this.counter = this.counter
+        }
+    },
+    computed: {
+        squareCounter(){
+            console.log('computed squareCounter')
+            return this.counter * this.counter
         }
     }
 };
