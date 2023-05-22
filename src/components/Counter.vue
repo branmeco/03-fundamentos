61e4746c9ba043b5e68a63a25c4e6c6e941844f5
branmeco <branmeco17@gmail.com>
2023-05-22 11:19:17 -0500

    Incrementar y Decrementar

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,35 +1,57 @@
 <template>
   <h2>Counter</h2>
-  <p>{{counter}} <sup>2</sup> = {{counter * counter}}</p>
-  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
-  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
-  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
-  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
-  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+  <div>
+    <button v-on:click="increase">+1</button>
+    <button v-on:click="decrease">-1</button>
+  </div>
 </template>
 
 <script>
 export default {
-    // name: 'Patito'
-    data(){
-        return {
-            counter: 5
-        }
+  // name: 'Patito'
+  data() {
+    return {
+      counter: 5,
+    };
+  },
+  methods: {
+    getSquareValue() {
+      console.log("getSquareValue");
+      return (this.counter = this.counter);
     },
-    methods: {
-        getSquareValue(){
-            console.log('getSquareValue')
-            return this.counter = this.counter
-        }
+    //increase
+    increase() {
+      this.counter++;
     },
-    computed: {
-        squareCounter(){
-            console.log('computed squareCounter')
-            return this.counter * this.counter
-        }
+    //decrease
+    decrease() {
+      this.counter--;
     }
+  },
+  computed: {
+    squareCounter() {
+      console.log("computed squareCounter");
+      return this.counter * this.counter;
+    },
+  },
 };
 </script>
 
 <style>
+button {
+  margin: 0 5px;
+  background-color: #64bbb7;
+  color: white;
+  border: 1px solid white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
 </style>
